35ee7fd5c7da8dc6bc2926636560def1cb8865ea
vipin <hduser@vipin-master>
2014-04-22 17:33:27 +0530

    refacotred R code

--- classification-tests/RcodeforDatgen.R
+++ classification-tests/RcodeforDatgen.R
@@ -2,43 +2,43 @@
 
 ### random Binomial distribution for the probability of success 1/2 /3 1/6 respectively
 
-U <- array(0,dim= c(10,30))
+	U <- array(0,dim= c(10,30))
 for(i in 1:10)
 {
-U[i,] <- rbinom(30,10,1/6)
+	U[i,] <- rbinom(30,10,1/6)
+} 
+	write(U, "/home/hduser/onebysix.txt",ncolumns=30,append=TRUE, sep = "\t")
+for(i in 1:10)
+{
+	U[i,] <- rbinom(30,10,1/6)
 } 
 write(U, "/home/hduser/onebysix.txt",ncolumns=30,append=TRUE, sep = "\t")
+
+
+
+
+	U <- array(0,dim= c(10,30))
 for(i in 1:10)
 {
-U[i,] <- rbinom(30,10,1/6)
+	U[i,] <- rbinom(30,10,1/2)
 } 
-write(U, "/home/hduser/onebysix.txt",ncolumns=30,append=TRUE, sep = "\t")
-
-
-
-
-U <- array(0,dim= c(10,30))
+	write(U, "/home/hduser/onebytwo.txt",ncolumns=30,append=TRUE, sep = "\t")
 for(i in 1:10)
 {
-U[i,] <- rbinom(30,10,1/2)
-} 
-write(U, "/home/hduser/onebytwo.txt",ncolumns=30,append=TRUE, sep = "\t")
-for(i in 1:10)
-{
-U[i,] <- rbinom(30,10,1/2)
+	U[i,] <- rbinom(30,10,1/2)
 } 
 write(U, "/home/hduser/onebytwo.txt",ncolumns=30,append=TRUE, sep = "\t")
 
 
-U <- array(0,dim= c(10,30))
+	U <- array(0,dim= c(10,30))
 for(i in 1:10)
 {
-U[i,] <- rbinom(30,10,1/3)
+	U[i,] <- rbinom(30,10,1/3)
 } 
-write(U, "/home/hduser/onebythree.txt",ncolumns=30,append=TRUE, sep = "\t")
+	write(U, "/home/hduser/onebythree.txt",ncolumns=30,append=TRUE, sep = "\t")
 for(i in 1:10)
 {
-U[i,] <- rbinom(30,10,1/3)
+	U[i,] <- rbinom(30,10,1/3)
 } 
 write(U, "/home/hduser/onebythree.txt",ncolumns=30,append=TRUE, sep = "\t")
 
