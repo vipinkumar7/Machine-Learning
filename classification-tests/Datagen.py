5eeb18a49dfa57d6ffec20053f1beabef1553246
vipin <hduser@vipin-master>
2014-04-22 14:17:36 +0530

    added new classsification tests

---
+++ classification-tests/Datagen.py
@@ -0,0 +1,4 @@
+U =np.zeros(300).reshape((10, 30))
+ for x in range(1,10):
+ U[x] = np.random.binomial(n, p, 30)
+
---
+++ classification-tests/RcodeforDatgen.R
@@ -0,0 +1,44 @@
+##@author Vipin Kumar
+
+### random Binomial distribution for the probability of success 1/2 /3 1/6 respectively
+
+U <- array(0,dim= c(10,30))
+for(i in 1:10)
+{
+U[i,] <- rbinom(30,10,1/6)
+} 
+write(U, "/home/hduser/onebysix.txt",ncolumns=30,append=TRUE, sep = "\t")
+for(i in 1:10)
+{
+U[i,] <- rbinom(30,10,1/6)
+} 
+write(U, "/home/hduser/onebysix.txt",ncolumns=30,append=TRUE, sep = "\t")
+
+
+
+
+U <- array(0,dim= c(10,30))
+for(i in 1:10)
+{
+U[i,] <- rbinom(30,10,1/2)
+} 
+write(U, "/home/hduser/onebytwo.txt",ncolumns=30,append=TRUE, sep = "\t")
+for(i in 1:10)
+{
+U[i,] <- rbinom(30,10,1/2)
+} 
+write(U, "/home/hduser/onebytwo.txt",ncolumns=30,append=TRUE, sep = "\t")
+
+
+U <- array(0,dim= c(10,30))
+for(i in 1:10)
+{
+U[i,] <- rbinom(30,10,1/3)
+} 
+write(U, "/home/hduser/onebythree.txt",ncolumns=30,append=TRUE, sep = "\t")
+for(i in 1:10)
+{
+U[i,] <- rbinom(30,10,1/3)
+} 
+write(U, "/home/hduser/onebythree.txt",ncolumns=30,append=TRUE, sep = "\t")
+
