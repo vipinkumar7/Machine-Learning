dcbd3ebb5ee57ab9c024f553cd19a2b4e973f041
Vipin Kumar <kvipin@athenahealth.com>
2018-04-12 17:55:38 +0530

    commited with PR curve

--- spend_classification/PredictL1.py
+++ spend_classification/PredictL1.py
@@ -12,7 +12,9 @@ from sklearn import  linear_model
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.preprocessing import label_binarize
 from sklearn import preprocessing
+from  PlotPrecisionRecallCurve import  PrecisionRecallCurve
 import hashlib
 
 
@@ -26,31 +28,12 @@ class PredictSpend():
     def generateHash(self,x):
         return int(hashlib.sha1(str(x).encode('utf-8')).hexdigest(), 16) % (10 ** 8)
 
-    def myfunc1(self,x):
-        if(x=='Non-Sourceable'):
-            return 1
-        elif (x=='T&E'):
-            return 2
-        elif(x=='Professional Services'):
-            return 3
-        elif(x=='FM'):
-            return 4
-        elif(x=='IT'):
-            return 5
-        elif(x=='Marketing'):
-            return 6
-        elif(x=='Unclassified'):
-            return 7
-        elif(x=='HR'):
-            return 8
-        elif(x=='Utilities'):
-            return 9
-        elif(x=='Logistics'):
-            return 10
+    def putUnknown(self,x):
+        if x and x.strip():
+            return x.lower()
         else:
-            return 0
+            return "unknown"
 
-        
     def predict(self):
         print("Loading data...")
         xl = pd.ExcelFile(self.inputFile)
@@ -64,7 +47,7 @@ class PredictSpend():
         Line_Type=training_data['Line Type'].apply(lambda x: self.generateHash (x) if np.all(pd.notnull(x)) else self.generateHash("unknown"))
         Dist_Desc=training_data['Dist Desc'].apply(lambda x: self.generateHash (x) if np.all(pd.notnull(x)) else self.generateHash("unknown"))
 
-        Y=training_data['L1'].apply(lambda x: self.myfunc1 (x))
+        Y=training_data['L1'].apply(lambda x: self.putUnknown(x))
         X = pd.DataFrame({'Division Desc': division_des,
                                   "Cost Centre Desc": CCDesc,
                                   "Nominal Des": NDesc,
@@ -72,21 +55,22 @@ class PredictSpend():
                                   "Dist Desc": Dist_Desc
                                   }
                                  )
-        #print(Y.isnull().sum())
         model = linear_model.LinearRegression() #XGBClassifier()
         seed =7
         test_size=0.33
-        X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=test_size,random_state=seed)
+        label_encoder = preprocessing.LabelEncoder()
+        label_encoder.fit(Y.unique())
+        classes = list(range(len(label_encoder.classes_)))
+        print(Y.unique())
+        Y_transformed = label_encoder.transform(Y)
+        Y_hat = label_binarize(Y_transformed, classes=classes)
 
+        X_train,X_test,Y_train,Y_test=train_test_split(X,Y_hat,test_size=test_size,random_state=seed)
         model.fit(X_train, Y_train)
-
-
         Y_prediction=model.predict(X_test)
-        accuracy=model.score(X_test,Y_prediction)
-        print("R2 Score for Model ::"+ str(accuracy))
-        print("Mean squared error: %.2f"
-          % mean_squared_error(Y_test,Y_prediction))
-        print('Variance score: %.2f' % r2_score(Y_test,Y_prediction))
+        plotting=PrecisionRecallCurve(Y_test,Y_prediction)
+        plotting.plot_curve()
+
 
 if __name__ == '__main__':
     p= PredictSpend("/Users/kvipin/Downloads/Invoice.xlsx")
